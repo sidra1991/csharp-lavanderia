8f5f73b6bff7b8b05de0be7b9379c06a8fba3402
anthony cecconi <anthony.cecconi.91@gmail.com>
2022-11-03 20:22:43 +0100

    punto 2 raggiunto

--- Program.cs
+++ Program.cs
@@ -16,43 +16,83 @@ Console.WriteLine(" 1 vedi stato lavatrici " );
 Console.WriteLine(" 2 vedi stato asciugatrici");
 Console.WriteLine(" 3 vedi stato di tutti i macchinari");
 Console.WriteLine(" 4 vedi tutti i lavaggi ");
+int selezioneMenu = Convert.ToInt32(Console.ReadLine());
 
 
-void menu() {
-    switch (Convert.ToInt32(Console.ReadLine()))
+void menu(int selezioneMenu) {
+
+    switch (selezioneMenu)
     {
         case 1:
             Console.WriteLine("stato lavatrici");
             lavanderia.MostraLavatrici();
             Console.WriteLine("vuoi maggiorni dettali ? si o no?");
-            if (Console.ReadLine == "si" || Console.ReadLine == "s"|| Console.ReadLine == "y"||Console.ReadLine == "yes")
+            if (Console.ReadLine() == "si" || Console.ReadLine() == "s"|| Console.ReadLine() == "y"||Console.ReadLine() == "yes")
             {
-                extraMenu()
+                int select = 1;
+                extraMenu(select);
             }
             break;
         case 2:
             Console.WriteLine("stato asciugatrici");
             lavanderia.MostraAsciugatrici();
+            Console.WriteLine("vuoi maggiorni dettali ? si o no?");
+            if (Console.ReadLine() == "si" || Console.ReadLine() == "s" || Console.ReadLine() == "y" || Console.ReadLine() == "yes")
+            {
+                int select = 2;
+                extraMenu(select);
+            }
             break;
         case 3:
             Console.WriteLine("stato macchinari");
             lavanderia.MostraAsciugatrici();
             lavanderia.MostraLavatrici();
+            Console.WriteLine("vuoi maggiorni dettali ? si o no?");
+            if (Console.ReadLine() == "si" || Console.ReadLine() == "s" || Console.ReadLine() == "y" || Console.ReadLine() == "yes")
+            {
+                int select = 0;
+                extraMenu(select);
+            }
             break;
         case 4:
             Console.WriteLine("lavaggi eseguiti da tutte le macchine");
             lavanderia.Mostralavaggi();
+            
             break;
 
         default:
-            menu();
+            menu(selezioneMenu);
             break;
     }
 }
 
+void extraMenu(int select){
+
+    switch (select)
+    { 
+        case 0:
+            Console.WriteLine("seleziona:");
+            Console.WriteLine("1. se vuoi le specifiche di una lavatrice");
+            Console.WriteLine("2. se vuoi le specifiche di una asciugatrice");
+            select = Convert.ToInt32(Console.ReadLine());
+            extraMenu(select);
+            break;
+        case 1:
+            Console.WriteLine("seleziona quale lavatrice");
+            lavanderia.MostraLavatrici();
+            lavanderia.dettagliLavatrice();
+            break;
+        case 2:
+            Console.WriteLine("seleziona quale lavatrice");
+            lavanderia.MostraAsciugatrici();
+            lavanderia.dettagliAsciugatrici();
+            break;
 
 
-menu();
+    }
+}
+
+menu(selezioneMenu);
 
 
 
@@ -105,6 +145,37 @@ class Lavanderia
             }
             Console.WriteLine((i+1) + " - " + lavatrici[i].Nome + stato + " lavvagio in corso " + lavatrici[i].AttualeLavaggio);
         }
+    }
+
+    public void dettagliLavatrice()
+    {
+        int Lava = Convert.ToInt32(Console.ReadLine()) - 1;
+        string stato = " utilizzabile";
+        if (lavatrici[Lava].Stato)
+        {
+            stato = " in uso";
+            Console.WriteLine("lavaggio in esecuzione " + lavatrici[Lava].AttualeLavaggio);
+        }
+        Console.WriteLine("nome della macchina " + lavatrici[Lava].Nome);
+        Console.WriteLine("stato della macchina " + stato);
+        Console.WriteLine("stato dell' ammorbidente residuo " + lavatrici[Lava].Ammorbidente);
+        Console.WriteLine("stato del detersivo residuo " + lavatrici[Lava].Detersivo);
+        Console.WriteLine("lavaggi effettuati " + lavatrici[Lava].lavaggio.Length);
+        
+    }
+    public void dettagliAsciugatrici()
+    {
+        int Lava = Convert.ToInt32(Console.ReadLine()) - 1;
+        string stato = " utilizzabile";
+        if (asciugatrici[Lava].Stato)
+        {
+            stato = " in uso";
+            Console.WriteLine("asciugatura in esecuzione " + asciugatrici[Lava].Attivita);
+        }
+        Console.WriteLine("nome della macchina " + asciugatrici[Lava].Nome);
+        Console.WriteLine("stato della macchina " + stato);
+        Console.WriteLine("asciugature effettuate " + asciugatrici[Lava].asciugature.Length);
+
     }
 
     public void MostraAsciugatrici()
@@ -215,14 +286,35 @@ class Lavaggio
 
 class Asciugatura
 {
+    public string Tipo { get; set; }
     public int CostoGettoni { get; set; }
     public int Tempo { get; set; }
+
+    public Asciugatura(int selezionato )
+    {
+        switch (selezionato)
+        {
+            case 1:
+                Tipo = "rapido";
+                CostoGettoni = 2;
+                Tempo = 30;
+                break;
+            case 2:
+                Tipo = "intensso";
+                CostoGettoni = 4;
+                Tempo = 60;
+                break;
+        }
+    }
 }
 class Asciugatrice
 {
     public string Nome { get; set; }
 
+    public string Attivita {get; set; }
     public bool Stato { get; set; }
+
+    public Asciugatura[] asciugature;
 
     public Asciugatrice(string nome)
     {
@@ -231,12 +323,23 @@ class Asciugatrice
         if (Rand % 2 == 1)
         {
             this.Stato = true;
+            int selezionato = new Random().Next(1, 3);
+            Asciugatura attuale = new Asciugatura(selezionato);
+            this.Attivita = attuale.Tipo;
         }
         else
         {
             this.Stato = false;
+            
         }
+        int arRand = new Random().Next(1, 10);
 
+        asciugature = new Asciugatura[arRand];
+        for (int i = 0; i < asciugature.Length; i++)
+        {
+            int selezionato = new Random().Next(1, 3);
+            this.asciugature[i] = new Asciugatura(selezionato);
+        }
     }
 
   

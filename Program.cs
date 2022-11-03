1b38a5ab4b18865ec44de0457e96981918bf106c
anthony cecconi <anthony.cecconi.91@gmail.com>
2022-11-03 14:53:33 +0100

    inizio a creare i metodi

--- Program.cs
+++ Program.cs
@@ -3,7 +3,9 @@
 
 // passo 1) inizio dallo sviluppare le classi coinvolte - lavanderia- lavatrici- asciugatrici.
 
-class lavanderia
+// passo 2) creazione delle lavatrici dalla classe genitore
+
+class Lavanderia
 {
     protected int GettoniUsati { get; set; }
     private float gettoniValore;
@@ -14,35 +16,76 @@ class lavanderia
 
     private int[] asciugatrici;
 
-    private lavanderia()
+    private Lavanderia()
     {
         this.gettoniValore = 0.50f;
         this.lavatrici = new int[5];
         this.asciugatrici = new int[5];
 
         // da aggiungere la creazine delle istanze per le lavatrici e le asciugatrici
+
+        for (int i = 0; i < (this.lavatrici.Length -1) ; i++)
+        {
+            string nome = "L" + Convert.ToString(i - 1);
+            Lavatrice lavatrice = new Lavatrice(nome);
+        }
+
+        for (int i = 0; i < (this.asciugatrici.Length - 1); i++)
+        {
+            string nome = "A" + Convert.ToString(i - 1);
+            Asciugatrice asciugatrice = new Asciugatrice(nome);
+        }
     }
 
     // metodi da aggiungere = vedere corrispettivo guadagnato- generare 24 ore di lavoro random - avere le info delle macchine
 
 }
 
-class lavatrice
+class Lavatrice
 {
     public string Nome { get; set; }
 
-    public bool stato { get; set; }
+    public bool Stato { get; set; }
+
+    public Lavatrice(string nome)
+    {
+        int Rand = new Random().Next( 1,5 ); 
+        this.Nome = nome;
+        if( Rand % 2 == 1)
+        {
+            this.Stato = true;
+        }
+        else
+        {
+            this.Stato = false;
+        }
+
+    }
 
 
     // metodi da aggiungere = inserire i 3 programmi di lavoro delle lavatrici
 }
 
-class asciugatrice
+class Asciugatrice
 {
     public string Nome { get; set; }
 
-    public bool stato { get; set; }
+    public bool Stato { get; set; }
 
+    public Asciugatrice(string nome)
+    {
+        int Rand = new Random().Next(1, 5);
+        this.Nome = nome;
+        if (Rand % 2 == 1)
+        {
+            this.Stato = true;
+        }
+        else
+        {
+            this.Stato = false;
+        }
+
+    }
 
     // metodi da aggiungere = inserire i 3 programmi di lavoro delle lavatrici
 }

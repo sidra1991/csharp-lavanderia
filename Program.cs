263dacd54d6e1e7f435a85b0af2207e263438c77
anthony cecconi <anthony.cecconi.91@gmail.com>
2022-11-03 15:28:30 +0100

    create interazioni tra le classi, ora devo creare un modo di uso su console

--- Program.cs
+++ Program.cs
@@ -37,6 +37,18 @@ class Lavanderia
         }
     }
 
+    public void UsoLavatrici(int gettoni, int detersivo , int ammorbidente)
+    {
+        this.GettoniUsati += gettoni;
+        this.DetersivoConsumato += detersivo;
+        this.AmmorbidenteConsumato += ammorbidente;
+
+    }
+    public void UsoAsciugatrici(int gettoni)
+    {
+        this.GettoniUsati += gettoni;
+    }
+
     // metodi da aggiungere = vedere corrispettivo guadagnato- generare 24 ore di lavoro random - avere le info delle macchine
 
 }
@@ -64,6 +76,61 @@ class Lavatrice
 
 
     // metodi da aggiungere = inserire i 3 programmi di lavoro delle lavatrici
+    //Rinfrescante, costo di 2 gettoni, durata di 20 minuti, consumo di 20ml di detersivo e 5ml di ammorbidente.
+    void Rinfrescante(Lavanderia lavanderia)
+    {
+        // TODO aggiungere orario di fine usando i 20 minuti
+        if (this.Stato == false) 
+        { 
+        this.Stato = !this.Stato;
+        }
+        else
+        {
+            Console.WriteLine("questa lavatrice è gia in uso");
+        }
+        int gettoni = 2;
+        int detersivo = 20;
+        int ammorbidente = 5;
+        lavanderia.UsoLavatrici(gettoni , detersivo , ammorbidente);
+        
+    }
+
+
+    //   Rinnovante, costo di 3 gettoni, durata di 40 minuti, consumo di 40ml di detersivo e 10ml di ammorbidente.
+    void Rinnovante(Lavanderia lavanderia)
+    {
+        // TODO aggiungere orario di fine usando i 40 minuti
+        if (this.Stato == false)
+        {
+            this.Stato = !this.Stato;
+        }
+        else
+        {
+            Console.WriteLine("questa lavatrice è gia in uso");
+        }
+        int gettoni = 3;
+        int detersivo = 40;
+        int ammorbidente = 10;
+        lavanderia.UsoLavatrici(gettoni, detersivo, ammorbidente);
+
+    }
+    //Sgrassante, costo di 4 gettoni, durata di 60 minuti, consumo di 60 ml di detersivo e 15ml di ammorbidente.
+    void Sgrassante(Lavanderia lavanderia)
+    {
+        // TODO aggiungere orario di fine usando i 60 minuti
+        if (this.Stato == false)
+        {
+            this.Stato = !this.Stato;
+        }else
+        {
+            Console.WriteLine("questa lavatrice è gia in uso");
+        }
+        int gettoni = 4;
+        int detersivo = 60;
+        int ammorbidente = 15;
+        lavanderia.UsoLavatrici(gettoni, detersivo, ammorbidente);
+
+    }
 }
 
 class Asciugatrice
@@ -88,4 +155,22 @@ class Asciugatrice
     }
 
     // metodi da aggiungere = inserire i 3 programmi di lavoro delle lavatrici
+    //, rapido 2 gettoni e intenso 4 gettoni della durata di 30 minuti e 60 minuti rispettivamente.
+
+    void Rapido(Lavanderia lavanderia)
+    {
+        // TODO aggiungere orario di fine usando i 30 minuti
+        if (this.Stato == false)
+        {
+            this.Stato = !this.Stato;
+        }
+        else
+        {
+            Console.WriteLine("questa lavatrice è gia in uso");
+        }
+        int gettoni = 2;
+        lavanderia.UsoAsciugatrici(gettoni);
+
+    }
+
 }
